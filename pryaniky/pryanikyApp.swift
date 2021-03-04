0df31edb0bd32620064c8a9301f6ed03d7a79cc6
Potopakhin Egor <potopakhin.ev@gmail.com>
2021-03-04 11:33:13 +0700

    Initial commit

---
+++ pryaniky/pryanikyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pryanikyApp.swift
+//  pryaniky
+//
+//  Created by Егор Потопахин on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct pryanikyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
